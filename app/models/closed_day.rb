814f6b276046ddffef1a514c32b8e4ecbe5b33d6
prokoninaritai <s0a7c5h1i@gmail.com>
2024-12-22 12:08:36 +0900

    バリデーションの追加

--- app/models/closed_day.rb
+++ app/models/closed_day.rb
@@ -1,3 +1,11 @@
 class ClosedDay < ApplicationRecord
   belongs_to :station
+
+   # 必須のフィールド
+   validates :start_date, presence: true
+   validates :end_date, presence: true
+   validates :closed_info, presence: true
+ 
+   # 同じ `station_id` で重複する期間を登録しない
+   validates :start_date, uniqueness: { scope: [:end_date, :station_id], message: "既に登録されています" }
 end
