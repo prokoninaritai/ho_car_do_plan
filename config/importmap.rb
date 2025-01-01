964d4aaaabbea8f7ebfe9936d0b89a6257425366
prokoninaritai <s0a7c5h1i@gmail.com>
2025-01-01 16:51:14 +0900

    bootstrap記述消去

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "dashboard", to: "dashboard.js"
 pin "itineraries", to: "itineraries.js"
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.min.js", preload: true
