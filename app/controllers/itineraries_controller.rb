1711ca8aae429b6f88a6b1c8549089d7172451a6
prokoninaritai <s0a7c5h1i@gmail.com>
2025-01-01 14:56:33 +0900

    Rubocop修正

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -2,7 +2,7 @@ class ItinerariesController < ApplicationController
   def create
     @itinerary = Itinerary.new(itinerary_params)
     if @itinerary.save
-      redirect_to root_path, notice: "旅程が登録されました！"
+      redirect_to root_path, notice: '旅程が登録されました！'
     else
       Rails.logger.debug "Turbo Stream Response: #{@itinerary.errors.full_messages}"
       respond_to do |format|
@@ -12,11 +12,9 @@ class ItinerariesController < ApplicationController
     end
   end
 
-
   private
 
   def itinerary_params
     params.require(:itinerary).permit(:title, :start_date, :end_date).merge(user_id: current_user.id)
   end
-
 end
--- test/controllers/itineraries_controller_test.rb
+++ test/controllers/itineraries_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class ItinerariesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
